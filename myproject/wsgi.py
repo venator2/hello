fa8a90afacd63e108f703ebce932bd258da93200
venator2 <bohdan.konon@gmail.com>
2024-08-06 22:33:20 +0300

    first commit

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Додайте шлях до вашого проекту
+path = '/home/venator3/myproject'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Встановіть змінну середовища для вашого проекту
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+
+# Активуйте віртуальне оточення
+activate_this = '/home/venator3/myproject/myenv/bin/activate_this.py'
+exec(open(activate_this).read(), {'__file__': activate_this})
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
 application = get_wsgi_application()
+
